ccb0b2c832fceea5155023f8d7e686f18fce509d
Deniz G <deniz.gunenc@toptal.com>
2024-11-23 14:49:39 +0300

    Add first test case Register User

--- tests/automation-exercise.e2e.test.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require("@playwright/test");
-
-test.describe("Automation Exercise", () => {
-  test("should load the homepage", async ({ page }) => {
-    await page.goto("/");
-    await expect(page).toHaveTitle("Automation Exercise");
-  });
-});
