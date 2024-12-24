14732aa345f7074a8e360f3a7f2fe66e2ff99b9d
Deniz G <deniz.gunenc@toptal.com>
2024-12-24 13:36:40 +0300

    Add Navigation tests

---
+++ tests/e2e/navigation.e2e.test.js
@@ -0,0 +1,69 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe.parallel("Navigation Tests", () => {
+  test.use({ testIdAttribute: "data-qa" });
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+    await expect(page).toHaveTitle(/Automation Exercise/i);
+  });
+
+  test("should navigate to test cases page", async ({ page }) => {
+    await test.step("Navigate to the test cases page", async () => {
+      await page.getByRole("link", { name: "Test Cases" }).first().click();
+      await expect(page).toHaveTitle(/Test Cases/i);
+      await expect(
+        page.getByRole("link", { name: /Test Case \d/ })
+      ).toHaveCount(26);
+    });
+  });
+
+  test("should scroll up with the scroll to top button", async ({ page }) => {
+    await expect(subscriptionHeading(page)).not.toBeInViewport();
+
+    await test.step("Scroll down", async () => {
+      await scrollDown(page);
+      await expect(subscriptionHeading(page)).toBeInViewport();
+    });
+
+    await test.step("Click on the scroll to top button", async () => {
+      await page.locator("#scrollUp").click();
+
+      await expect(subscriptionHeading(page)).not.toBeInViewport();
+      await expect(mainHeading(page)).toBeInViewport();
+    });
+  });
+
+  test("should scroll up and down without the scroll to top button", async ({
+    page,
+  }) => {
+    await expect(subscriptionHeading(page)).not.toBeInViewport();
+
+    await test.step("Scroll down", async () => {
+      await scrollDown(page);
+      await expect(subscriptionHeading(page)).toBeInViewport();
+    });
+
+    await test.step("Scroll up", async () => {
+      await scrollUp(page);
+      await expect(subscriptionHeading(page)).not.toBeInViewport();
+      await expect(mainHeading(page)).toBeInViewport();
+    });
+  });
+});
+
+const subscriptionHeading = (page) => {
+  return page.getByRole("heading", { name: "Subscription", exact: true });
+};
+
+const mainHeading = (page) => {
+  return page.getByRole("heading", { name: "Full-Fledged practice website" });
+};
+
+const scrollDown = async (page) => {
+  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+};
+
+const scrollUp = async (page) => {
+  await page.evaluate(() => window.scrollTo(0, 0));
+};
